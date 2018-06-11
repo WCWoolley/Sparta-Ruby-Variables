a104609b1d351143957c1631c88f78471a51d2ad
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-06-11 11:47:28 +0100

    lab 1

---
+++ app.rb
@@ -0,0 +1,80 @@
+# my_number = 5
+# my_first_string = "Some string here"
+# a_slightly_longer_variable = 100.5
+#
+# puts my_number
+# puts my_first_string
+# puts a_slightly_longer_variable.ceil
+
+# NUMBERS
+
+# age = 29.0
+# current_year = 2018
+# year_born = current_year - age
+#
+# age_in_ten_years = age + 10
+# double_my_age = age * 2
+# num_decades_old = age / 10.0
+
+# puts num_decades_old.to_int
+# puts num_decades_old.to_f
+
+# STRINGS
+
+# my_first_name = "Will"
+# my_last_name = "Cooper"
+#
+# my_full_name = "#{my_first_name} #{my_last_name}"
+#
+# puts my_full_name
+# puts my_full_name.reverse
+# puts my_full_name.length
+# puts my_full_name.upcase
+# puts my_full_name.downcase
+
+# SYMBOLS
+
+# my_name = "Will" # String
+# puts my_name.class
+#
+# my_name = :Will # Symbol
+# puts my_name.class
+
+# ARRAYS
+
+# arr_of_numbers = [1,2,3,4,5]
+# mixed_array = ["Will", :name, arr_of_numbers, 29]
+#
+# p mixed_array
+# p mixed_array.length
+#
+# mixed_array.pop # remove from end AND change array
+# mixed_array.push 45 # add to end AND change array
+#
+# mixed_array.shift # remove from beginning AND change array
+# mixed_array.unshift 45 # add to beginning AND change array
+#
+# p mixed_array
+#
+# p mixed_array[2].last
+
+# HASHES
+
+# first_hash = {
+#   name: "Will",
+#   age: 25
+# }
+
+# first_hash = {
+#   :name => "Will",
+#   :age => 25
+# }
+#
+# puts first_hash[:name]
+# puts first_hash.methods
+
+# arr = [1,2,3]
+#
+# arr.reverse!
+#
+# puts arr
